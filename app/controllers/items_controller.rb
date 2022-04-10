eb459945940624641fdb4abcc3d910c665649ff5
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-10 17:40:17 -0600

    add: edit action and update action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,8 +6,25 @@ class ItemsController < ApplicationController
   end
 
   def show
-    # require "pry"
-    # binding.pry
+    @merchant = Merchant.find(params[:id])
     @item = Item.find(params[:id])
+  end
+
+  def edit
+    @merchant = Merchant.find(params[:id])
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    merchant = Merchant.find(params[:merchant_id])
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to "/merchants/#{merchant.id}/items/#{item.id}", notice: "Item Successfully Updated"
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price)
   end
 end
