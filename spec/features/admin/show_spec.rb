6b5b94777bbafd8fdeb7fe2ea96e7e3ee667390a
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-12 13:24:16 -0600

    add test for links

--- spec/features/admin/show_spec.rb
+++ spec/features/admin/show_spec.rb
@@ -6,4 +6,11 @@ RSpec.describe "the admin dashboard" do
 
     expect(page).to have_content("Welcome to Admin Dashboard")
   end
+
+  it "shows links to access merchant and invoice index" do
+    visit "/admin"
+
+    expect(page).to have_link("Merchants Index")
+    expect(page).to have_link("Invoices Index")
+  end
 end
