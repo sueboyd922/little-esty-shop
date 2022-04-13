256da59130228c03a7688e8cb10bffed59c7d8f7
Susan Boyd <sueboyd922@gmail.com>
2022-04-13 16:03:47 -0400

    Test: refactor admin/invoice/index spec after weird test data incident

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe 'admin invoices index page' do
   it 'shows all the invoices with a link to their show page' do
-    @invoice1 = Invoice.all.sample
-    @invoice2 = Invoice.all.sample
-    @invoice3 = Invoice.all.sample
-    @invoice4 = Invoice.all.sample
-    @invoice5 = Invoice.all.sample
-    require "pry"; binding.pry
-    invoices = [@invoice1, @invoice2, @invoice3, @invoice4, @invoice5]
+    @customer = Customer.create(first_name: "Sally", last_name: "Jones")
 
-    invoices.each do |invoice|
+    @invoice1 = @customer.invoices.create!(status: 0)
+    @invoice2 = @customer.invoices.create!(status: 1)
+    @invoice3 = @customer.invoices.create!(status: 0)
+    @invoice4 = @customer.invoices.create!(status: 2)
+    @invoice5 = @customer.invoices.create!(status: 1)
+
+    Invoice.all.each do |invoice|
       visit "/admin/invoices"
       click_on("#{invoice.id}")
       expect(current_path).to eq("/admin/invoices/#{invoice.id}")
