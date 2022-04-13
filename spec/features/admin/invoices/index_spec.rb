c05898c834fe32a4ae0619e57009768b692f124e
Susan Boyd <sueboyd922@gmail.com>
2022-04-12 20:12:10 -0400

    Mess with rspec

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe 'admin invoices index page' do
     @invoice3 = Invoice.all.sample
     @invoice4 = Invoice.all.sample
     @invoice5 = Invoice.all.sample
-    # require "pry"; binding.pry
+    require "pry"; binding.pry
     invoices = [@invoice1, @invoice2, @invoice3, @invoice4, @invoice5]
 
     invoices.each do |invoice|
       visit "/admin/invoices"
       click_on("#{invoice.id}")
-      expect(current_path).to eq("/admin/invoices/#{invoice.id}") 
+      expect(current_path).to eq("/admin/invoices/#{invoice.id}")
     end
   end
 end
