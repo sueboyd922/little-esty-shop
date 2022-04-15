a19ed8da45641e664a6fe63ed2f44adeab555f8c
Kim G <xiaole.guo@yahoo.com>
2022-04-14 20:32:15 -0600

    build successful transactions count method in customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,11 @@
 class Customer < ApplicationRecord
   has_many :invoices
-
+  has_many :transactions, through: :invoices 
   validates_presence_of :first_name, :last_name
 
+  def successful_transactions_count
+    Customer.joins(invoices: :transactions)
+        .where("transactions.result = 0 AND invoices.status = 2 AND customers.id = #{self.id}")
+        .count
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Customer, type: :model do
 
   describe 'associations' do
 	  it {should have_many :invoices}
+    it { should have_many(:transactions).through(:invoices)}
   end
 
   describe 'validations' do
@@ -11,4 +12,19 @@ RSpec.describe Customer, type: :model do
     it { should validate_presence_of(:last_name)}
   end
 
+  describe 'instance methods' do 
+    describe '.successful_transactions_count' do 
+      it 'returns the number of successful transactions' do 
+          FactoryBot.create_list(:customer, 7) 
+          cust1 = Customer.all[0]
+          invoice1 = FactoryBot.create_list(:invoice, 1, customer_id: cust1.id, status: 2)[0]
+          invoice2 = FactoryBot.create_list(:invoice, 1, customer_id: cust1.id, status: 0)[0]
+          FactoryBot.create_list(:transaction, 10, invoice_id: invoice1.id, result: 0) 
+          FactoryBot.create_list(:transaction, 2, invoice_id: invoice1.id, result: 1) 
+          FactoryBot.create_list(:transaction, 2, invoice_id: invoice2.id, result: 0) 
+          
+          expect(cust1.successful_transactions_count).to eq(10)
+      end
+    end
+  end
 end
