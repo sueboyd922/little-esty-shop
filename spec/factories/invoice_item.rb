195b46f1ad64a667809379b6fd77a0c1476777f1
Kim G <xiaole.guo@yahoo.com>
2022-04-14 20:33:15 -0600

    add more factories model for different classes

--- spec/factories/invoice_item.rb
+++ spec/factories/invoice_item.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     association :item
     association :invoice
     status {[0, 1, 2].sample}
+    quantity { Faker::Number.within(range: 1..10)}
+    unit_price { Faker::Number.within(range: 1..100_000)}
     id { Faker::Number.unique.within(range: 1..1_000_000)}
   end
 end
