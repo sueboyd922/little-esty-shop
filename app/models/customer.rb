a364df0a17c167ca7ef632b6a81b494fce8c4b66
Kim G <xiaole.guo@yahoo.com>
2022-04-14 22:59:08 -0600

    refactor -delete customer transaction count method as it was not necessary

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,5 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices 
   validates_presence_of :first_name, :last_name
 
-  def successful_transactions_count
-    Customer.joins(invoices: :transactions)
-        .where("transactions.result = 0 AND invoices.status = 2 AND customers.id = #{self.id}")
-        .count
-  end
+ 
 end
