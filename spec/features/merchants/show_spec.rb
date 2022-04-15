7eaa76d9f543888478c5e6c8cbb6f9a3afad5859
Kim G <xiaole.guo@yahoo.com>
2022-04-14 20:33:38 -0600

    show page shows the name and number of successful transactions of top 5 customers

--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -62,4 +62,80 @@ RSpec.describe 'merchant show page' do
 			expect(page).to have_content(@invoice1.created_at.strftime("%A, %B %-d, %Y"))
 		end
 	end
+
+	it 'returns the top five customers with largest number of successful ransactions' do 
+		item1 = FactoryBot.create_list(:item, 1, merchant_id: @merchant1.id)[0]
+		FactoryBot.create_list(:customer, 7) 
+        cust1 = Customer.all[0]
+        cust2 = Customer.all[1]
+        cust3 = Customer.all[2]
+        cust4 = Customer.all[3]
+        cust5 = Customer.all[4]
+        cust6 = Customer.all[5]
+        cust7 = Customer.all[6]
+        
+        invoice1 = FactoryBot.create_list(:invoice, 1, customer_id: cust5.id, status: 2)[0]
+        invoice2 = FactoryBot.create_list(:invoice, 1, customer_id: cust3.id, status: 2)[0]
+        invoice3 = FactoryBot.create_list(:invoice, 1, customer_id: cust1.id, status: 2)[0]
+        invoice4 = FactoryBot.create_list(:invoice, 1, customer_id: cust6.id, status: 2)[0]
+        invoice5 = FactoryBot.create_list(:invoice, 1, customer_id: cust7.id, status: 0)[0]
+        invoice6 = FactoryBot.create_list(:invoice, 1, customer_id: cust2.id, status: 2)[0]
+        invoice7 = FactoryBot.create_list(:invoice, 1, customer_id: cust4.id, status: 2)[0]
+
+
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice1.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice2.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice3.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice4.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice5.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice6.id)
+         FactoryBot.create_list(:invoice_item, 1, item_id: item1.id, invoice_id: invoice7.id)
+ 
+ 
+        FactoryBot.create_list(:transaction, 10, invoice_id: invoice1.id, result: 0) 
+        FactoryBot.create_list(:transaction, 8, invoice_id: invoice2.id, result: 0) 
+        FactoryBot.create_list(:transaction, 7, invoice_id: invoice3.id, result: 0) 
+        FactoryBot.create_list(:transaction, 6, invoice_id: invoice4.id, result: 0) 
+        FactoryBot.create_list(:transaction, 4, invoice_id: invoice5.id, result: 0) 
+        FactoryBot.create_list(:transaction, 2, invoice_id: invoice6.id, result: 0) 
+        FactoryBot.create_list(:transaction, 1, invoice_id: invoice7.id, result: 0) 
+       
+		visit "/merchants/#{@merchant1.id}/dashboard"
+       save_and_open_page
+		within("#top_5-#{cust5.id}") do 
+			expect(page).to have_content(cust5.first_name)
+			expect(page).to have_content(cust5.last_name)
+			expect(page).to have_content(10)
+		end
+
+		within("#top_5-#{cust3.id}") do 
+			expect(page).to have_content(cust3.first_name)
+			expect(page).to have_content(cust3.last_name)
+			expect(page).to have_content(8)
+		end
+
+		within("#top_5-#{cust1.id}") do 
+			expect(page).to have_content(cust1.first_name)
+			expect(page).to have_content(cust1.last_name)
+			expect(page).to have_content(7)
+		end
+
+
+		within("#top_5-#{cust6.id}") do 
+			expect(page).to have_content(cust6.first_name)
+			expect(page).to have_content(cust6.last_name)
+			expect(page).to have_content(6)
+		end
+
+		within("#top_5-#{cust2.id}") do 
+			expect(page).to have_content(cust2.first_name)
+			expect(page).to have_content(cust2.last_name)
+			expect(page).to have_content(2)
+		end
+
+		expect(page).to_not have_content(cust7.first_name)
+		expect(page).to_not have_content(cust7.last_name)
+		expect(page).to_not have_content(cust4.first_name)
+		expect(page).to_not have_content(cust4.last_name)
+	end
 end
