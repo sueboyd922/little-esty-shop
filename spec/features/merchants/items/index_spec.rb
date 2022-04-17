bdda84ad0344435f0931101882a475971e9ed83e
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-17 12:32:26 -0600

    test: feat: top 5 popular_items

--- spec/features/merchants/items/index_spec.rb
+++ spec/features/merchants/items/index_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe "merchants items index page", type: :feature do
     expect(page).not_to have_content(@item5.name)
   end
 
-
   it "has a link to create a new item" do
     visit "/merchants/#{@merchant1.id}/items"
 
@@ -30,11 +29,11 @@ RSpec.describe "merchants items index page", type: :feature do
     expect(current_path).to eq("/merchants/#{@merchant1.id}/items/new")
   end
 
-  it 'has the ability to enable or disable an item' do
+  it "has the ability to enable or disable an item" do
     visit "/merchants/#{@merchant1.id}/items"
 
     expect(@item1.status).to eq("enabled")
-    
+
     within ".item-#{@item1.id}" do
       click_on "Disable"
     end
@@ -59,7 +58,7 @@ RSpec.describe "merchants items index page", type: :feature do
     end
   end
 
-  it 'groups items by disabled or enabled items' do
+  it "groups items by disabled or enabled items" do
     visit "/merchants/#{@merchant1.id}/items"
 
     expect(page).to have_content("Enabled Items")
@@ -73,5 +72,73 @@ RSpec.describe "merchants items index page", type: :feature do
     within ".disabled" do
       expect(page).to have_content(@item3.name)
     end
+  end
+
+  it "section with top 5 popular_items" do
+    merchant_1 = Merchant.create!(name: "Stuff and Things ")
+    item_1 = FactoryBot.create_list(:item, 1, merchant_id: merchant_1.id)[0]
+    item_2 = FactoryBot.create_list(:item, 2, merchant_id: merchant_1.id)[1]
+    item_3 = FactoryBot.create_list(:item, 3, merchant_id: merchant_1.id)[2]
+    item_4 = FactoryBot.create_list(:item, 4, merchant_id: merchant_1.id)[3]
+    item_5 = FactoryBot.create_list(:item, 5, merchant_id: merchant_1.id)[4]
+    item_6 = FactoryBot.create_list(:item, 6, merchant_id: merchant_1.id)[5]
+    item_7 = FactoryBot.create_list(:item, 7, merchant_id: merchant_1.id)[6]
+
+    FactoryBot.create_list(:customer, 7)
+    cust1 = Customer.all[0]
+    cust2 = Customer.all[1]
+    cust3 = Customer.all[2]
+    cust4 = Customer.all[3]
+    cust5 = Customer.all[4]
+    cust6 = Customer.all[5]
+    cust7 = Customer.all[6]
+
+    invoice1 = FactoryBot.create_list(:invoice, 1, customer_id: cust1.id, status: 2)[0]
+    invoice2 = FactoryBot.create_list(:invoice, 2, customer_id: cust2.id, status: 2)[1]
+    invoice3 = FactoryBot.create_list(:invoice, 3, customer_id: cust3.id, status: 2)[2]
+    invoice4 = FactoryBot.create_list(:invoice, 4, customer_id: cust4.id, status: 2)[3]
+    invoice5 = FactoryBot.create_list(:invoice, 5, customer_id: cust5.id, status: 0)[4]
+    invoice6 = FactoryBot.create_list(:invoice, 6, customer_id: cust6.id, status: 2)[5]
+    invoice7 = FactoryBot.create_list(:invoice, 7, customer_id: cust7.id, status: 2)[6]
+
+    FactoryBot.create_list(:invoice_item, 1, item_id: item_1.id, invoice_id: invoice1.id)
+    FactoryBot.create_list(:invoice_item, 2, item_id: item_2.id, invoice_id: invoice2.id)
+    FactoryBot.create_list(:invoice_item, 3, item_id: item_3.id, invoice_id: invoice3.id)
+    FactoryBot.create_list(:invoice_item, 4, item_id: item_4.id, invoice_id: invoice4.id)
+    FactoryBot.create_list(:invoice_item, 5, item_id: item_5.id, invoice_id: invoice5.id)
+    FactoryBot.create_list(:invoice_item, 6, item_id: item_6.id, invoice_id: invoice6.id)
+    FactoryBot.create_list(:invoice_item, 7, item_id: item_7.id, invoice_id: invoice7.id)
+
+    FactoryBot.create_list(:transaction, 1, invoice_id: invoice1.id, result: 1)
+    FactoryBot.create_list(:transaction, 2, invoice_id: invoice2.id, result: 0)
+    FactoryBot.create_list(:transaction, 3, invoice_id: invoice3.id, result: 0)
+    FactoryBot.create_list(:transaction, 4, invoice_id: invoice4.id, result: 0)
+    FactoryBot.create_list(:transaction, 5, invoice_id: invoice5.id, result: 1)
+    FactoryBot.create_list(:transaction, 6, invoice_id: invoice6.id, result: 0)
+    FactoryBot.create_list(:transaction, 7, invoice_id: invoice7.id, result: 0)
+    visit "/merchants/#{merchant_1.id}/items"
+    expect(page).to have_content("Top 5 Items by Revenue")
+
+    within ".top_items" do
+      expect(page).to have_link(item_7.name)
+      expect(page).to have_link(item_6.name)
+      expect(page).to have_link(item_4.name)
+      expect(page).to have_link(item_3.name)
+      expect(page).to have_link(item_2.name)
+
+      expect(item_7.name).to appear_before(item_6.name)
+      expect(item_6.name).to appear_before(item_4.name)
+      expect(item_4.name).to appear_before(item_2.name)
+
+      # expect(page).to have_content("19777219")
+      # expect(page).to have_content("9899016")
+      # expect(page).to have_content("3737512")
+      # expect(page).to have_content("1897324")
+      # expect(page).to have_content("1244265")
+    end
+    within ".top_items" do
+      click_link(item_2.name)
+    end
+    expect(current_path).to eq("/merchants/#{merchant_1.id}/items/#{item_2.id}")
   end
 end
