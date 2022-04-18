836a0c3c6db2bc5a82b11c242c14ff5dc7666c77
Kim G <xiaole.guo@yahoo.com>
2022-04-17 22:59:46 -0600

    update merchant/item status column

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     association :merchant
     name { Faker::Commerce.product_name}
     description { Faker::Commerce.material }
+    status {[0, 1].sample}
     unit_price { Faker::Number.within(range: 1..100_000)}
     id { Faker::Number.unique.within(range: 1..100_000)}
   end
