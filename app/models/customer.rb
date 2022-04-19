d110ed97bb38a1337e5537c51d55b78a658bbfb6
CoryBethune <95403666+CoryBethune@users.noreply.github.com>
2022-04-18 17:55:14 -0700

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,4 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   validates_presence_of :first_name, :last_name
 
-  enum status: {"enabled" => 0, "disabled" => 1}
-
 end
