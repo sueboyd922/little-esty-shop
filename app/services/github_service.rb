a0c7e90e7bd13f7dca2ffa84adbc01c97ea49845
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-20 16:19:01 -0600

    repo name api

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,16 +1,17 @@
-require 'json'
-require 'httparty'
+require "json"
+require "httparty"
 
 class GithubService
+  def repo
+    get_url("https://api.github.com/repos/sueboyd922/little-esty-shop")
+  end
 
-	def contributors
-		get_url("https://api.github.com/repos/sueboyd922/little-esty-shop/contributors")
-	end
+  def contributors
+    get_url("https://api.github.com/repos/sueboyd922/little-esty-shop/contributors")
+  end
 
-
-	def get_url(url)
-		response = HTTParty.get(url)
-		JSON.parse(response.body, symbolize_names: true)
-	end
-
+  def get_url(url)
+    response = HTTParty.get(url)
+    json = JSON.parse(response.body, symbolize_names: true)
+  end
 end
