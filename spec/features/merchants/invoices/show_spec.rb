6780c42adccca5034a812accca5952298605e320
Susan Boyd <sueboyd922@gmail.com>
2022-04-24 23:22:56 -0400

    Test: Invoice Item having link to their discounts

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -54,5 +54,47 @@ RSpec.describe 'merchant invoices show page' do
     expect(page).not_to have_content("Total Revenue After Discount: $384.50")
   end
 
+  it 'shows a link to the discount show page if the item has a discount applied' do
+    merchant1 = Merchant.create!(name: "Joyce's Things")
+    merchant2 = Merchant.create!(name: "Samwise's Things")
+
+    item1 = merchant1.items.create!(name: "Bowtie", description: "it looks cool", unit_price: 1200)
+    item2 = merchant1.items.create!(name: "Earrings", description: "it looks cool", unit_price: 2000)
+
+    item3 = merchant2.items.create!(name: "Stuffed elephant", description: "fun for kids", unit_price: 1550)
+
+    customer = Customer.create!(first_name: "Jamie", last_name: "Personname")
+
+    invoice = customer.invoices.create(status: "in progress")
+
+    invoice_item1 = InvoiceItem.create!(item: item1, invoice: invoice, quantity: 10, unit_price: 1300, status: "packaged")
+    invoice_item2 = InvoiceItem.create!(item: item2, invoice: invoice, quantity: 3, unit_price: 2000, status: "shipped")
+    invoice_item3 = InvoiceItem.create!(item: item3, invoice: invoice, quantity: 16, unit_price: 1550, status: "pending")
+
+    discount = merchant1.discounts.create!(quantity: 5, percent_discount: 10)
+    discount2 = merchant2.discounts.create!(quantity: 15, percent_discount: 15)
+
+
+    visit "merchants/#{merchant1.id}/invoices/#{invoice.id}"
+  
+    within "#invoice_item-#{invoice_item1.id}" do
+      expect(page).not_to have_content("Discounts: None")
+      click_on("Applied")
+      expect(current_path).to eq("/merchants/#{merchant1.id}/discounts/#{discount.id}")
+    end
+
+    # within ".invoice_item-#{invoice_item3.id}" do
+    #   visit "merchants/#{merchant1.id}/invoices/#{invoice.id}"
+    #   expect(page).not_to have_content("Discounts: None")
+    #   click_on("Applied")
+    #   expect(current_path).to eq("/merchants/#{merchant2.id}/discounts/#{discount2.id}")
+    # end
+
+    visit "merchants/#{merchant1.id}/invoices/#{invoice.id}"
+    within ".invoice_item-#{invoice_item2.id}" do
+      expect(page).not_to have_button("Applied")
+      expect(page).to have_content("Discounts: None")
+    end
+  end
 
 end
