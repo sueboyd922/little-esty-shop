349359afbe7b9471ac0621c572c83d7a8970a302
Susan Boyd <sueboyd922@gmail.com>
2022-04-26 12:38:57 -0400

    Test: Admin Invoice Revenue After Discount

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -99,4 +99,38 @@ RSpec.describe "admin invoices show page" do
     expect(page).to have_field(:status, with: "completed")
     expect(current_path).to eq("/admin/invoices/#{invoice5.id}")
   end
+
+  it 'shows the discounted revenue of an invoice' do
+    customer = Customer.create!(first_name: "Jessie", last_name: "Thomas")
+
+    invoice = customer.invoices.create!(status: 0)
+
+    merchant1 = Merchant.create!(name: "Patsy's Pies")
+    merchant2 = Merchant.create!(name: "Jungle Jims")
+    merchant3 = Merchant.create!(name: "Holistic Holly")
+
+    item1 = merchant1.items.create!(name: "Applie Pie", description: "apple", unit_price: 1300)
+    item2 = merchant1.items.create!(name: "Pumpkin Pie", description: "Pumpkin", unit_price: 1300)
+
+    item3 = merchant2.items.create!(name: "Swingset", description: "fun", unit_price: 22300)
+
+    item4 = merchant3.items.create!(name: "Incense", description: "nose smells", unit_price: 600)
+    item5 = merchant3.items.create!(name: "Lighter", description: "fire", unit_price: 1000)
+
+    InvoiceItem.create!(item: item1, invoice: invoice, quantity: 5, unit_price: 1300, status: 1)
+    InvoiceItem.create!(item: item2, invoice: invoice, quantity: 10, unit_price: 1300, status: 1)
+    InvoiceItem.create!(item: item3, invoice: invoice, quantity: 1, unit_price: 22300, status: 1)
+    InvoiceItem.create!(item: item4, invoice: invoice, quantity: 15, unit_price: 600, status: 1)
+    InvoiceItem.create!(item: item5, invoice: invoice, quantity: 3, unit_price: 1000, status: 1)
+
+    discount1 = merchant1.discounts.create!(quantity: 5, percent_discount: 10)
+    discount2 = merchant1.discounts.create!(quantity: 10, percent_discount: 15)
+    discount3 = merchant2.discounts.create!(quantity: 2, percent_discount: 50)
+    discount4 = merchant3.discounts.create!(quantity: 5, percent_discount: 10)
+    # require "pry"; binding.pry
+
+    visit "/admin/invoices/#{invoice.id}"
+    expect(page).to have_content("Total Revenue: $538.00")
+    expect(page).to have_content("Total Revenue After Discount: $503.00")
+  end
 end
